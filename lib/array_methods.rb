654da706b8aded95cd4f8feabbea27c6f2284967
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-04 05:42:32 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,32 @@
-ef find_element_index(array, value_to_find)
-  
-  
+def find_element_index(array, value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+  nil
 end
 
 def find_max_value(array)
- 
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
 end
 
 def find_min_value(array)
-  puts find_min_value(array)
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
 end
-scale = [2,3,4,5]
-  scale_2 = [6,5,4,3]
-  hill = [1,2,3,4,5,4,3,2,1]
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  zig_zag = [500,4,1000,5,250]
