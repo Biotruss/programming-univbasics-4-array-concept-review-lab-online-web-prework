b58bac512b5d0abbc138fa5540d2bad1fd661813
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-04 05:41:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-ef find_element_index(array, value_to_find)
+def find_element_index(array, value_to_find)
   
   
 end
@@ -15,3 +15,5 @@ scale = [2,3,4,5]
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
+  
+  
